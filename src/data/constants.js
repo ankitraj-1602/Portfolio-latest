3eaeb0c67e3e301adaea1e5d9bb1727da29c2054
ankitraj-1602 <nycankit@gmail.com>
2024-09-03 10:12:10 +0530

    portfolio components

---
+++ src/data/constants.js
@@ -0,0 +1,263 @@
+export const Bio = {
+  name: "Ankit Raj",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a full-stack MERN (MongoDB, Express, React, Node.js) developer and DSA (Data Structures and Algorithms) coding enthusiast. I am also passionate about data structures and algorithms. I enjoy solving complex programming problems and enhancing my skills in this area. My proficiency in DSA helps me design efficient and optimized code for various applications.",
+  github: "https://github.com/ankitraj-1602",
+  resume:
+    "https://drive.google.com/file/d/1euuWF3dg2EOHA_YpJozUUzmuFXRoJxUL/view?usp=drive_link",
+  // linkedin: "https://linkedin.com/in/ankitrj1602",
+  // twitter: "https://twitter.com/RishavChanda",
+  // insta: "https://www.instagram.com/rishav_chanda/",
+  // facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "ANT Design",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbDeLPMs8R6c5alio7_RUTogKsjbg6j5SW6A&s",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRruDIY6Y1GIuZH42S2HVoh0fgt18fdh5D8iw&s",
+      
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+
+    ],
+  },
+  {
+    title: "Programming And ML",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "C",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://www.vikingsoftware.com/wp-content/uploads/2024/02/C-2.png",
+      },
+      {
+        name: "NumPy",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRC2C9EVtvZjW_wQ3f9bEP2Fgla230C3kVYQ&s",
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://netdata.cloud/img/pandas.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+         "https://static-00.iconduck.com/assets.00/git-icon-2048x2048-juzdf1l5.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/postman.svg",
+      },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/C4E0BAQH1mBe2JkQkbQ/company-logo_200_200/company-logo_200_200/0/1632903673637?e=1733356800&v=beta&t=vAp9662l8S-dcEi25wLq9W5eqiW668oIx_1i6Xg3Djc",
+    role: "Software Engineer",
+    company: "AGILE ICO PVT LTD",
+    date: " October 2023 – Present",
+    desc: "Leveraged the MERN stack to build scalable enterprise applications, including a robust School Management System with authentication, role-based access, and dashboards for admin, student, and faculty. Enhanced document management with SharePoint using automated workflows. Collaborated effectively with teams to achieve project success.",
+    skills: [
+      "MERN Stack",
+      "ANT Design",
+      "SharePoint",
+      "PowerApps",
+      "Power Automate",
+      "Git",
+    ],
+    // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+
+  {
+    id: 1,
+    img: "https://miro.medium.com/v2/resize:fit:1400/0*JlnxyNcvZuZ0k1vp",
+    role: "Software Developer Engineer Intern",
+    company: "Desi QnA",
+    date: "Oct 2022 – Dec 2022",
+    desc: "Designed and developed a fully responsive website for optimal performance across devices. Implemented a hashmap for efficient domain management and user validation. Created dynamic, user-friendly web elements using JavaScript and Bootstrap, enhancing interaction and functionality. Optimized front-end code and layout for improved responsiveness and performance.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "BootStrap",
+      "php",
+      "MySQL",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://www.giet.edu/wp-content/uploads/2020/08/University-Logo-backup.png?c56598&c56598",
+    school: "Gandhi Institute for Education and Technology, Gunupur",
+    date: "Oct 2020 - May 2024",
+    grade: "9.35 CGPA",
+    // desc: "I have completed a Bachelor's degree in Computer Science and Engineering at Gandhi Institute for Education and Technology, Gunupur. I have completed 8 semesters and have a CGPA of 9.35. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at GIETU, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://scontent.fblr11-1.fna.fbcdn.net/v/t39.30808-1/302554406_445372037610750_283130548304636045_n.png?stp=dst-png_s200x200&_nc_cat=107&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=z7JFnD_lPSAQ7kNvgGg2pf2&_nc_ht=scontent.fblr11-1.fna&_nc_gid=Amfk72MsKndD_77zIxRXTC0&oh=00_AYCeKN0-EBiuIbXeTUvgcIvJssJ6iNXnAX-iibAGYKah9g&oe=66DBB213",
+    school: "Inter Science College, Hazaribagh ,Jharkhand",
+    date: "Apr 2018 - Apr 2020",
+    grade: "85%",
+    // desc: "I have completed my class 12 high school education at Inter Science Colleg, Hazaribagh ,Jharkhand, where I studied Science with Computer Science.",
+    degree: "JAC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://scontent-bom1-1.xx.fbcdn.net/v/t39.30808-1/302145018_451432010355859_8949085123886149168_n.png?stp=c0.0.280.280a_dst-png_s200x200&_nc_cat=104&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=LHWdiDbpJw8Q7kNvgHfL8rr&_nc_ht=scontent-bom1-1.xx&oh=00_AYA5tpPkoSZ2zhrxJ9ty5yrUkenvRRAIM0KJv-thKqVaxg&oe=66DBA181",
+    school: "A.H.S, Hazaribagh ,Jharkhand",
+    date: "Apr 2017 - Apr 2018",
+    grade: "89.6%",
+    // desc: "I have completed my class 10 education at A.H.S, Hazaribagh ,Jharkhand, where I studied Science with Computer Application.",
+    degree: "JAC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Edutech Platform",
+    // date: "Apr 2023 - May 2023",
+    description:
+      "Developed secure backend functionalities, including authentication, role management, and permission control. Managed three dashboards (admin, student, faculty) for tailored data access and user interaction. Integrated nine modules (admission, fees, accounts, timetable, examination, stocks, payroll, admin) with features like bulk ID card downloads for comprehensive school management.",
+    image:
+      "https://i.postimg.cc/tCkCrjhS/Whats-App-Image-2024-09-02-at-23-58-56.jpg",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    // github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://aicopldemos.net/",
+    // member: [
+    //   {
+    //     name: "Rishav Chanda",
+    //     img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+    //     linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    //     github: "https://github.com/rishavchanda/",
+    //   },
+    //   {
+    //     name: "Upasana Chaudhuri",
+    //     img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+    //     linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //     github: "https://github.com/upasana0710",
+      // },
+    // ],
+  },
+  {
+    id: 1,
+    title: "Mentor Finder",
+    // date: "Oct 2022 - Present",
+    description:
+      "Developed a platform for students to search and book mentors based on skills and experience. Integrated Stripe for secure payments and automated mailing for booking confirmations and receipts. Implemented features like date selection, mentor filtering, and booking management, allowing users to easily manage and cancel appointments.",
+    image:
+      "https://i.postimg.cc/3JY2438n/Whats-App-Image-2024-09-02-at-23-59-06.jpg",
+    tags: [
+      "React Js",
+      "MongoDb",
+      "Node Js",
+      "Express Js",
+    ],
+    category: "web app",
+    webapp: "https://mentorfinder.netlify.app/",
+  },
+  // {
+  //   id: 2,
+  //   title: "Dribbble Clone",
+  //   // date: "Jan 2023 - Mar 2023",
+  //   description:
+  //     "Developed a Dribbble clone using Next.js with secure user authentication and project management. Implemented features for users to log in, post, edit, and delete projects, replicating Dribbble's functionality to provide a realistic user experience.",
+  //   image:
+  //     "https://github.com/rishavchanda/Brain-Tumor-Detection/raw/main/Readme_resource/Image2.png",
+  //   tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+  //   category: "web app",
+  //   // github: "https://github.com/rishavchanda/Brain-Tumor-Detection",
+  //   webapp: "https://brain-tumor.netlify.app/",
+ 
+  // },  
+];
